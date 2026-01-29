27fd8e24469bd26b87842b26a37ce2c29e47a32c
kwat0g <181074004+kwat0g@users.noreply.github.com>
2026-01-29 20:48:19 +0800

    new

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
-from fastapi import FastAPI, Request, Response
+from fastapi import FastAPI, Request
+from fastapi.responses import Response
 from messenger import verify_webhook, handle_message
 
 app = FastAPI()
 
-
 @app.get("/webhook")
-def webhook_verify(request: Request):
+async def webhook_verify(request: Request):
     return verify_webhook(request)
-
 
 @app.post("/webhook")
 async def webhook_receive(request: Request):
--- messenger.py
+++ messenger.py
@@ -2,19 +2,21 @@ from fastapi import Response
 import requests
 from config import VERIFY_TOKEN, PAGE_ACCESS_TOKEN, ALLOWED_SENDER_ID
 from commands import process_command
+from fastapi.responses import PlainTextResponse
+import os
 
 
-def verify_webhook(request):
+def verify_webhook(request: Request):
     params = request.query_params
+
     mode = params.get("hub.mode")
     token = params.get("hub.verify_token")
     challenge = params.get("hub.challenge")
 
     if mode == "subscribe" and token == VERIFY_TOKEN:
-        # IMPORTANT: return plain text, not JSON
-        return Response(content=challenge, media_type="text/plain")
+        return PlainTextResponse(content=challenge, status_code=200)
 
-    return Response(content="Verification failed", status_code=403)
+    return PlainTextResponse(content="Verification failed", status_code=403)
 
 
 async def handle_message(payload: dict):
